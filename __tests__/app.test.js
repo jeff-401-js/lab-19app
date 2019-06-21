82b98b81423807896277a3f54e62c578ed2eaeef
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-20 19:16:23 -0700

    one test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const utils = require('../app/utils.js');
-const logger = require('../logger/utilsLogger.js');
+const utils = require('../utils.js');
+
 
 jest.mock('fs');
 
@@ -10,32 +10,6 @@ describe('uppercase', () => {
     let str = 'some test string';
     str = utils.upper(str);
     expect(str).toEqual('SOME TEST STRING');
-  });
-});
-
-describe('log save function', () => {
-  it('should take in a message and console.log it', () => {
-    let test = {
-      name: 'saved',
-      data: `SAVED: event just happened!`,
-    };
-    let spy = jest.spyOn(console, 'log');
-    logger.logSave(JSON.stringify(test));
-    expect(spy).toHaveBeenCalled();
-    spy.mockRestore();
-  });
-});
-
-describe('log error function', () => {
-  it('should take in a message and console.error it', () => {
-    let test = {
-      name: 'error',
-      data: `ERROR: event just happened!`,
-    };
-    let spy = jest.spyOn(console, 'error');
-    logger.logError(JSON.stringify(test));
-    expect(spy).toHaveBeenCalled();
-    spy.mockRestore();
   });
 });
 
